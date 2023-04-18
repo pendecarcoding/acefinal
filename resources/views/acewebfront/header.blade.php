47a0fa47a97638dedb50fc15f0f1f34d4d446fa8
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-04-18 13:42:12 +0800

    update

--- resources/views/acewebfront/header.blade.php
+++ resources/views/acewebfront/header.blade.php
@@ -3,9 +3,35 @@
          @foreach(getnav() as $v)
         <a href="{{ url($v['link']) }}" @if(Request::is($v['link'].'*'))class="active"@endif>{{ $v['name'] }}</a>
         @endforeach
-        <a class="ace-button" href="https://gtp.ace2u.com/" target="_blank">Login GTP</a>
+        <!-- <a style="padding-left: 26px;
+        padding-right: 26px;
+        padding-top: 2px;
+        padding-bottom: 2px;" class="ace-button" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Login
+        </a>
+
+          <!-- Create div with dropdown-menu class and ID that matches data-bs-target attribute on button -->
+          <!-- <div style="overflow: hidden;padding-right: 20px;" class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a style="color:black;" class="dropdown-item" href="#">GTP Login</a>
+            <a style="color:black;" class="dropdown-item" href="#" style="width:100%;">Stuff Login</a>
+
+          </div> --> -->
+
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown button
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+
         <a href="javascript:void(0);" class="icon" onclick="myFunction()">
           <i class="fa fa-bars"></i>
         </a>
       </nav>
     </header>
+
+
