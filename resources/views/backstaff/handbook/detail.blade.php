bd2ec12fb63703bbe4919a311ab18a428a2c4fa8
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-05-11 09:06:01 +0800

    update

--- resources/views/backstaff/handbook/detail.blade.php
+++ resources/views/backstaff/handbook/detail.blade.php
@@ -56,8 +56,8 @@ onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
 		var adobeDCView = new AdobeDC.View({clientId: "0a6dbb8538a84e74b342da5fdef1a6d0", divId: "adobe-dc-view"});
 		adobeDCView.previewFile({
-			content:{location: {url: "https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf"}},
-			metaData:{fileName: "Handbook AIAB.pdf"}
+			content:{location: {url: "https://www.ace2u.com/public/download/file_20230403055321.pdf"}},
+			metaData:{fileName: "file_20230403055321.pdf"}
 		}, {showAnnotationTools: false, showDownloadPDF: false, showPrintPDF: false});
 	});
 </script>
