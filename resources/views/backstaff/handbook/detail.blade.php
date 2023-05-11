3b21e58f5c2946bf016fd3ade70e2262d4705971
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-05-11 09:10:24 +0800

    update

--- resources/views/backstaff/handbook/detail.blade.php
+++ resources/views/backstaff/handbook/detail.blade.php
@@ -50,15 +50,17 @@ width="100%"
 height="780"
 style="border:none;" id="fraDisabled"
 onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
-<div id="adobe-dc-view" style="width:100%;height:800px"></div>
+
+
+<div id="adobe-dc-view"></div>
 <script src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
 <script type="text/javascript">
 	document.addEventListener("adobe_dc_view_sdk.ready", function(){ 
 		var adobeDCView = new AdobeDC.View({clientId: "0a6dbb8538a84e74b342da5fdef1a6d0", divId: "adobe-dc-view"});
 		adobeDCView.previewFile({
-			content:{location: {url: "https://www.ace2u.com/public/download/file_20230403055321.pdf"}},
-			metaData:{fileName: "file_20230403055321.pdf"}
-		}, {showAnnotationTools: false, showDownloadPDF: false, showPrintPDF: false});
+			content:{promise: "<FILE_BLOB_PROMISE>"},
+			metaData:{fileName: "<FILE_NAME>"}
+		}, {});
 	});
 </script>
 
