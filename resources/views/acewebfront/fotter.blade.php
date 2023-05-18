d35fc517ab67ab3e0a470b880bf73a6522e78188
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-05-18 10:32:22 +0800

    update a footer

--- resources/views/acewebfront/fotter.blade.php
+++ resources/views/acewebfront/fotter.blade.php
@@ -109,9 +109,7 @@
                     <a class="text-reset fw-bold" href="#">@ Ace Innovate Asia Berhad</a>
                     </div>
                     <div  class="col-md-6 col-sm-12 col-lg-6">
-
-                       <div style="float:right"> <a href="{{url('view_pdf/terms')}}" target="_blank"  class="text-reset fw-bold">Privacy notice | </a> <a href="{{url('view_pdf/product')}}" target="_blank"  class="text-reset fw-bold"> Terms of use | </a>  <a href="{{url('view_pdf/terms')}}" target="_blank"  class="text-reset fw-bold"> Terms of purchase</a></div>
-
+                       <div style="float:right"> <a href="{{url('view_pdf/terms')}}" target="_blank"  class="text-reset fw-bold">Privacy Notice & Terms of Purchase | </a> <a href="{{url('view_pdf/product')}}" target="_blank"  class="text-reset fw-bold"> Terms of use</a></div>
                     </div>
             </div>
 
