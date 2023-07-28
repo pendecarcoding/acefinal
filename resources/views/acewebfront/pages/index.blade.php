3ca304fe514048dfdb37ee094da705668396ef22
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-07-28 08:27:58 +0800

    update index landing page

--- resources/views/acewebfront/pages/index.blade.php
+++ resources/views/acewebfront/pages/index.blade.php
@@ -15,14 +15,14 @@
                     <div class="card-corporate">
                         <a href="{{ url('ourcompany') }}">
                             <center><img class="img-icon-corporate"
-                                    src="{{ asset('public/aceweb/assets/img/corporate.png') }}"></center>
+                                    src="{{ asset('public/aceweb/assets/img/corporate2.png') }}"></center>
                             <!-- <b>CORPORATE</b> -->
                         </a>
                     </div>
                     <div class="card-personal">
                         <a href="{{ url('forcorporate') }}">
                             <center><img class="img-icon-personal"
-                                    src="{{ asset('public/aceweb/assets/img/personal.png') }}"></center>
+                                    src="{{ asset('public/aceweb/assets/img/personal2.png') }}"></center>
                             <!-- <b>PERSONAL</b> -->
                         </a>
                     </div>
