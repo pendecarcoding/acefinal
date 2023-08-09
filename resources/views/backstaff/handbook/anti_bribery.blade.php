bfc557bdc0d78cb2b36fa8df0e42c35c1fdc1c8b
LAPTOP-38AI9LB7\silve <bohati@silverstream.my>
2023-08-09 16:40:26 +0800

    update clientID PDF for goinno

--- resources/views/backstaff/handbook/anti_bribery.blade.php
+++ resources/views/backstaff/handbook/anti_bribery.blade.php
@@ -1,12 +1,11 @@
 @extends('acewebfront.layouts')
 @section('content')
-<script type="text/javascript">
-    document.addEventListener("contextmenu", function(e){
-      e.preventDefault();
-    }, false);
-  </script>
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
-</script>
+    <script type="text/javascript">
+        document.addEventListener("contextmenu", function(e) {
+            e.preventDefault();
+        }, false);
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <main id="my-pdf">
 
@@ -33,36 +32,51 @@
 
 
                     <div class="col-sm-9" style="padding: 36px;">
-<!--
-                        if the pdf file does not appear you can use the following link : <a target="_blank" href="https://docs.google.com/viewer?url=https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf&embedded=true">Link PDF</a> -->
+                        <!--
+                            if the pdf file does not appear you can use the following link : <a target="_blank" href="https://docs.google.com/viewer?url=https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf&embedded=true">Link PDF</a> -->
                         <div class="well" style="margin-top:50px">
                             <!-- <iframe src="https://docs.google.com/viewer?url=https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf&embedded=true" style="width:100%; height:100vh;" frameborder="0"></iframe> -->
                             <!-- <embed
-                                src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf"
-                                style="width:100%;height: 85vh;" type="application/pdf"> -->
-<!--
-                                <embed src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf" type="application/pdf" width="600" height="780"
-                                oncontextmenu="return false" onkeydown="return false" onmousedown="return false"
-                                onselectstart="return false"></embed> -->
-                                <!-- <iframe
-src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf#toolbar=0"
-width="100%"
-height="780"
-style="border:none;" id="fraDisabled"
-onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
-<div id="adobe-dc-view" style="width:100%;height:800px"></div>
-<br>
-<input value="1" type="checkbox" name="allow_handbook" onclick="disableCheckbox(this)" id="myCheckbox"  {{checkagree('antibribery')}}> &nbsp;By clicking here, I state that I have read and understood the  Anti-Bribery and Corruption Policy
-<script src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
-<script type="text/javascript">
-	document.addEventListener("adobe_dc_view_sdk.ready", function(){
-		var adobeDCView = new AdobeDC.View({clientId: "f47ddb205d3849698c91458a8740eb8d", divId: "adobe-dc-view"});
-		adobeDCView.previewFile({
-			content:{location: {url: "/public/aceweb/pdf/anti_bribery.pdf"}},
-			metaData:{fileName: "."}
-		}, {showAnnotationTools: false, showDownloadPDF: false, showPrintPDF: false});
-	});
-</script>
+                                    src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf"
+                                    style="width:100%;height: 85vh;" type="application/pdf"> -->
+                            <!--
+                                    <embed src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf" type="application/pdf" width="600" height="780"
+                                    oncontextmenu="return false" onkeydown="return false" onmousedown="return false"
+                                    onselectstart="return false"></embed> -->
+                            <!-- <iframe
+    src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf#toolbar=0"
+    width="100%"
+    height="780"
+    style="border:none;" id="fraDisabled"
+    onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
+                            <div id="adobe-dc-view" style="width:100%;height:800px"></div>
+                            <br>
+                            <input value="1" type="checkbox" name="allow_handbook" onclick="disableCheckbox(this)"
+                                id="myCheckbox" {{ checkagree('antibribery') }}> &nbsp;By clicking here, I state that I have
+                            read and understood the  Anti-Bribery and Corruption Policy
+                            <script src="https://acrobatservices.adobe.com/view-sdk/viewer.js"></script>
+                            <script type="text/javascript">
+                                document.addEventListener("adobe_dc_view_sdk.ready", function() {
+                                    var adobeDCView = new AdobeDC.View({
+                                        clientId: "339ce5ce4878433f81df309c93ea2d8d",
+                                        divId: "adobe-dc-view"
+                                    });
+                                    adobeDCView.previewFile({
+                                        content: {
+                                            location: {
+                                                url: "/public/aceweb/pdf/anti_bribery.pdf"
+                                            }
+                                        },
+                                        metaData: {
+                                            fileName: "."
+                                        }
+                                    }, {
+                                        showAnnotationTools: false,
+                                        showDownloadPDF: false,
+                                        showPrintPDF: false
+                                    });
+                                });
+                            </script>
 
                         </div>
 
@@ -80,43 +94,42 @@ onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
     </main>
     <script>
         function disableCheckbox(checkbox) {
-          if (checkbox.checked) {
-            checkbox.disabled = true;
-          }
+            if (checkbox.checked) {
+                checkbox.disabled = true;
+            }
         }
 
         //SEND TO SERVER
         $(document).ready(function() {
-      // Set up CSRF token for Ajax requests
+            // Set up CSRF token for Ajax requests
             $.ajaxSetup({
                 headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
 
             $('#myCheckbox').on('change', function() {
                 var isChecked = $(this).is(':checked');
                 var data = {
-                isChecked: isChecked,
-                type:'antibribery'
+                    isChecked: isChecked,
+                    type: 'antibribery'
                 };
 
                 $.ajax({
-                url: "{{route('agree.staff')}}",
-                type: 'POST',
-                data: data,
-                success: function(response) {
-                    location.reload();
-                    // window.location.href = "{{route('staff.ethic')}}";
-                    console.log(response);
-                },
-                error: function(xhr) {
-                    // Handle any errors that occur during the request
-                    console.log(xhr.responseText);
-                }
+                    url: "{{ route('agree.staff') }}",
+                    type: 'POST',
+                    data: data,
+                    success: function(response) {
+                        location.reload();
+                        // window.location.href = "{{ route('staff.ethic') }}";
+                        console.log(response);
+                    },
+                    error: function(xhr) {
+                        // Handle any errors that occur during the request
+                        console.log(xhr.responseText);
+                    }
                 });
             });
-            });
-      </script>
-
+        });
+    </script>
 @endsection
--- resources/views/backstaff/handbook/detail.blade.php
+++ resources/views/backstaff/handbook/detail.blade.php
@@ -33,22 +33,22 @@
 
                     <div class="col-sm-9" style="padding: 36px;">
                         <!--
-                                if the pdf file does not appear you can use the following link : <a target="_blank" href="https://docs.google.com/viewer?url=https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf&embedded=true">Link PDF</a> -->
+                                    if the pdf file does not appear you can use the following link : <a target="_blank" href="https://docs.google.com/viewer?url=https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf&embedded=true">Link PDF</a> -->
                         <div class="well" style="margin-top:50px;">
                             <!-- <iframe src="https://docs.google.com/viewer?url=https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf&embedded=true" style="width:100%; height:100vh;" frameborder="0"></iframe> -->
                             <!-- <embed
-                                        src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf"
-                                        style="width:100%;height: 85vh;" type="application/pdf"> -->
+                                            src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf"
+                                            style="width:100%;height: 85vh;" type="application/pdf"> -->
                             <!--
-                                        <embed src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf" type="application/pdf" width="600" height="780"
-                                        oncontextmenu="return false" onkeydown="return false" onmousedown="return false"
-                                        onselectstart="return false"></embed> -->
+                                            <embed src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf" type="application/pdf" width="600" height="780"
+                                            oncontextmenu="return false" onkeydown="return false" onmousedown="return false"
+                                            onselectstart="return false"></embed> -->
                             <!-- <iframe
-        src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf#toolbar=0"
-        width="100%"
-        height="780"
-        style="border:none;" id="fraDisabled"
-        onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
+            src="https://corpweb.ace2u.com/public/uploads/all/heeL4Nh2Ce6OQt6hJBF0SrRQQn6hSTGG8rJqWdNt.pdf#toolbar=0"
+            width="100%"
+            height="780"
+            style="border:none;" id="fraDisabled"
+            onload="disableContextMenu();" onMyLoad="disableContextMenu();"> </iframe> -->
                             <div id="adobe-dc-view" style="width:100%;height:800px;position:relative">
 
                             </div>
@@ -60,13 +60,13 @@
                             <script type="text/javascript">
                                 document.addEventListener("adobe_dc_view_sdk.ready", function() {
                                     var adobeDCView = new AdobeDC.View({
-                                        clientId: "f47ddb205d3849698c91458a8740eb8d",
+                                        clientId: "339ce5ce4878433f81df309c93ea2d8d",
                                         divId: "adobe-dc-view"
                                     });
                                     adobeDCView.previewFile({
                                         content: {
                                             location: {
-                                                url: "/public/handbook/{{$handbook->file}}"
+                                                url: "/public/handbook/{{ $handbook->file }}"
                                             }
                                         },
                                         metaData: {
